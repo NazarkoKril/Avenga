c8db0044c15b232044b82f6f615f51304dafe3c7
NazarkoKril <122899742+NazarkoKril@users.noreply.github.com>
2024-08-10 14:59:08 +0300

    update

--- js/index.js
+++ js/index.js
@@ -145,3 +145,166 @@ const swiper3 = new Swiper(".review__swiper", {
     //     }
     //   }
 });
+
+// validation form 
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const phone = document.getElementById('phone');
+    const message = document.getElementById('message');
+    const agreement = document.getElementById('agreement');
+
+    let isValid = true;
+
+    const isNameValid = (name) => /^[a-zA-Zа-яА-ЯїЇєЄіІ]+(\s[a-zA-Zа-яА-ЯїЇєЄіІ]+)?$/.test(name.value.trim());
+
+    if (name.value.trim() === '' || !isNameValid(name)) {
+        isValid = false;
+        name.classList.add('error');
+        name.classList.remove('valid');
+    } else {
+        name.classList.remove('error');
+        name.classList.add('valid');
+    }
+
+    const isEmailValid = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value.trim());
+
+    if (email.value.trim() === '' || !isEmailValid(email)) {
+        isValid = false;
+        email.classList.add('error');
+        email.classList.remove('valid');
+    } else {
+        email.classList.remove('error');
+        email.classList.add('valid');
+    }
+
+    if (phone.value.trim() === '') {
+        isValid = false;
+        phone.classList.add('error');
+        phone.classList.remove('valid');
+    } else {
+        phone.classList.remove('error');
+        phone.classList.add('valid');
+    }
+
+    if (message.value.trim() === '') {
+        isValid = false;
+        message.classList.add('error');
+        message.classList.remove('valid');
+    } else {
+        message.classList.remove('error');
+        message.classList.add('valid');
+    }
+
+    if (!agreement.checked) {
+        isValid = false;
+        document.querySelector('.checkbox').classList.add('error');
+    } else {
+        document.querySelector('.checkbox').classList.remove('error');
+    }
+
+    if (isValid) {
+        alert('The form has been sent successfully!');
+        name.value = '';
+        email.value = '';
+        phone.value = '';
+        message.value = '';
+        agreement.checked = false;
+
+        name.classList.remove('valid');
+        email.classList.remove('valid');
+        phone.classList.remove('valid');
+        message.classList.remove('valid');
+        document.querySelector('.checkbox').classList.remove('error');
+    }
+});
+
+['input', 'change'].forEach(eventType => {
+    document.querySelectorAll('input, textarea').forEach(input => {
+        input.addEventListener(eventType, () => {
+            if (input.value.trim() !== '') {
+                input.classList.remove('error');
+                input.classList.add('valid');
+            } else {
+                input.classList.remove('valid');
+                input.classList.add('error');
+            }
+        });
+    });
+});
+
+// pop up local
+
+document.addEventListener('DOMContentLoaded', function () {
+    const popup = document.getElementById('popup');
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+
+    document.body.appendChild(overlay);
+
+    const popupAccepted = localStorage.getItem('popupAccepted');
+
+    if (!popupAccepted) {
+        showPopup();
+    }
+
+    document.querySelector('.active__btn').addEventListener('click', function () {
+        closePopup();
+    });
+
+    function showPopup() {
+        popup.classList.add('active');
+        overlay.classList.add('active');
+        document.body.classList.add('no-scroll');
+    }
+
+    function closePopup() {
+        popup.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+        localStorage.setItem('popupAccepted', 'true');
+    }
+});
+
+//search
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchIcon = document.getElementById('search-icon');
+    const searchContainer = document.getElementById('search-container');
+    const overlay = document.getElementById('overlay');
+
+    searchIcon.addEventListener('click', function (event) {
+        event.stopPropagation();
+        toggleSearch();
+    });
+
+    overlay.addEventListener('click', closeSearch);
+
+    document.addEventListener('click', function (event) {
+        if (!searchContainer.contains(event.target)) {
+            closeSearch();
+        }
+    });
+
+    function toggleSearch() {
+        const isActive = searchContainer.classList.contains('active');
+        if (!isActive) {
+            showSearch();
+        } else {
+            closeSearch();
+        }
+    }
+
+    function showSearch() {
+        searchContainer.classList.add('active');
+        overlay.classList.add('active');
+    }
+
+    function closeSearch() {
+        searchContainer.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+});
